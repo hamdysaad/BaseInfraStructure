0295db1ce61ffc3842511ee75c407a24d66782a1
Hamdy <hamdysaad506@gmail.com>
2020-03-12 20:23:03 +0200

    add baseActivityVM & BaseFragmentVM

--- infrastructurebase/src/main/java/com/mte/infrastructurebase/base/BaseViewModel.kt
+++ infrastructurebase/src/main/java/com/mte/infrastructurebase/base/BaseViewModel.kt
@@ -11,34 +11,8 @@ open class BaseViewModel : ViewModel() , KoinComponent{
 
     var activity: Activity? = null
     var context: Context? = null
-    protected  var lifecycleOwner: LifecycleOwner? = null
-    protected val fragment : BaseFragment<*>? = null
 
-    fun init(lifecycleOwner: LifecycleOwner){
-        this.lifecycleOwner = lifecycleOwner
-    }
+    var fragment : BaseFragment<*>? = null
 
-
-
-    fun addFragment(
-        fragment: Fragment,
-        id: Int,
-        addToBackStack: Boolean
-    ) {
-
-       this.fragment?.addFragment(fragment , id , addToBackStack)
-    }
-
-    fun replaceFragment(
-        fragment: Fragment,
-        id: Int,
-        addToBackStack: Boolean
-    ) {
-
-        this.fragment?.replaceFragment(fragment , id , addToBackStack)
-
-    }
-
-    
 
 }
